a5f24feb2994a7282b97c03be5f92ba9f8e83fb9
Andreas Taube <andreas.taube@collect.ai>
2016-12-28 22:30:21 +0100

    enable eslint

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-
-module.exports = function(table, values) {
-  if (!table) throw new Error('No table name defined');
-  if (!values) throw new Error('No values defined');
-
-  const isArray = Array.isArray(values);
-  const fields = isArray && values.length > 0 ? 
-    Object.keys(values[0]) : Object.keys(values);
-  const params = isArray ? 
-    getParamsForArray(fields, values) : getParamsForObject(values);
-  
-  const sql = `INSERT INTO ${table} (${fields.map(f => f)}) VALUES ${params};`
-  
-  return sql;
-}
-
 function getParamsForObject(obj) {
-  return `(${Object.values(obj).map((_,i) => `$${i + 1}`)})`;
+  return `(${Object.values(obj).map((_, i) => `$${i + 1}`)})`;
 }
 
 function getParamsForArray(fields, arr) {
   return arr.reduce((memo) => {
-    const next = memo.length * fields.length + 1;
+    const next = (memo.length * fields.length) + 1;
     memo.push(`(${fields.map((_, i) => `$${i + next}`)})`);
     return memo;
   }, []);
 }
+
+module.exports = function bulkInsert(table, values) {
+  if (!table) throw new Error('No table name defined');
+  if (!values) throw new Error('No values defined');
+
+  const isArray = Array.isArray(values);
+  const fields = isArray && values.length > 0 ?
+    Object.keys(values[0]) : Object.keys(values);
+  const params = isArray ?
+    getParamsForArray(fields, values) : getParamsForObject(values);
+
+  const sql = `INSERT INTO ${table} (${fields.map(f => f)}) VALUES ${params};`;
+
+  return sql;
+};
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,29 +1,29 @@
-var expect = require('unexpected');
-var bulkInsert = require('..');
+const expect = require('unexpected');
+const bulkInsert = require('..');
 
 describe('Bulk Insert Generator', () => {
   [
-    { 
+    {
       context: 'with simple object',
-      input: [ 'myTable', { name: 'Ben', age: 20 } ],
-      expect: `INSERT INTO myTable (name,age) VALUES ($1,$2);`
+      input: ['myTable', { name: 'Ben', age: 20 }],
+      expect: 'INSERT INTO myTable (name,age) VALUES ($1,$2);',
     },
-    { 
+    {
       context: 'with simple object 2',
-      input: [ 'myTable', { name: 'Ben', age: 20, city: 'Hamburg', country: 'Germany' } ],
-      expect: `INSERT INTO myTable (name,age,city,country) VALUES ($1,$2,$3,$4);`
+      input: ['myTable', { name: 'Ben', age: 20, city: 'Hamburg', country: 'Germany' }],
+      expect: 'INSERT INTO myTable (name,age,city,country) VALUES ($1,$2,$3,$4);',
     },
-    { 
+    {
       context: 'with array of objects',
-      input: [ 'myTable', [{ name: 'Ben', age: 20 }, { name: 'Michael', age: 30 }] ],
-      expect: `INSERT INTO myTable (name,age) VALUES ($1,$2),($3,$4);`
-    }
-  ].forEach(spec => {
+      input: ['myTable', [{ name: 'Ben', age: 20 }, { name: 'Michael', age: 30 }]],
+      expect: 'INSERT INTO myTable (name,age) VALUES ($1,$2),($3,$4);',
+    },
+  ].forEach((spec) => {
     context(spec.context, () => {
       it(spec.expect, () => {
-        const sql = bulkInsert.apply(null, spec.input);
-        expect(sql, 'to equal', spec.expect)
+        const sql = bulkInsert(...spec.input);
+        expect(sql, 'to equal', spec.expect);
       });
-    })
+    });
   });
 });
