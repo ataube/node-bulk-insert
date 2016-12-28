44828dfe90f6eeae61eace0a80767507f7aea931
Andreas Taube <andreas.taube@collect.ai>
2016-12-28 23:06:07 +0100

    handle invalid input

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getParamsForObject(obj) {
   return `(${Object.values(obj).map((_, i) => `$${i + 1}`)})`;
 }
 
-function getParamsForArray(fields, arr) {
+function getParamsForArray(arr, fields) {
   return arr.reduce((memo) => {
     const next = (memo.length * fields.length) + 1;
     memo.push(`(${fields.map((_, i) => `$${i + next}`)})`);
@@ -10,15 +10,23 @@ function getParamsForArray(fields, arr) {
   }, []);
 }
 
-module.exports = function bulkInsert(table, values) {
-  if (!table) throw new Error('No table name defined');
-  if (!values) throw new Error('No values defined');
+/**
+ * Generates a bulk insert SQL statement.
+ * If a list of objects is given all of them must have the same structure.
+ *
+ * @param {string} table - the table name
+ * @param {Object|Array} values - a object or array of objects
+ * @returns {string|null}
+ */
+module.exports = function bulkInsert(table = '', values = []) {
+  if (!table || table === '') return null;
+  if (!values || values.length === 0) return null;
 
   const isArray = Array.isArray(values);
-  const fields = isArray && values.length > 0 ?
+  const fields = isArray ?
     Object.keys(values[0]) : Object.keys(values);
   const params = isArray ?
-    getParamsForArray(fields, values) : getParamsForObject(values);
+    getParamsForArray(values, fields) : getParamsForObject(values);
 
   const sql = `INSERT INTO ${table} (${fields.map(f => f)}) VALUES ${params};`;
 
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -18,9 +18,24 @@ describe('Bulk Insert Generator', () => {
       input: ['myTable', [{ name: 'Ben', age: 20 }, { name: 'Michael', age: 30 }]],
       expect: 'INSERT INTO myTable (name,age) VALUES ($1,$2),($3,$4);',
     },
+    {
+      context: 'with null arguments',
+      input: [null, null],
+      expect: null,
+    },
+    {
+      context: 'with empty values',
+      input: ['myTable', []],
+      expect: null,
+    },
+    {
+      context: 'with empty table name',
+      input: ['', [{ name: 'Ben' }]],
+      expect: null,
+    },
   ].forEach((spec) => {
     context(spec.context, () => {
-      it(spec.expect, () => {
+      it(spec.expect || 'returns null', () => {
         const sql = bulkInsert(...spec.input);
         expect(sql, 'to equal', spec.expect);
       });
